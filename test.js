86042e0c35556546da916736a3dd24afe5a7caf3
stefan <stefan.e@swell.wtf>
2017-09-17 21:58:35 +0200

    - added test cases for object iteration

--- test.js
+++ test.js
@@ -11,12 +11,17 @@ const loops = 10000000;
 const testArray = [...new Array(loops).keys()];
 testArray[loops - 1] = 'findme';
 
-logger.log('testing on testArray with length', testArray.length);
+const testObject = testArray.reduce((result, item, index) => {
+    result[index] = item;
+    return result;
+}, {});
+
+logger.log('testing on testArray and testObject with length', testArray.length);
 
 const isInArrayForIn = (array, value) => {
     const result = [];
-    for (let item in array) {
-        if (array[item] === value) result.push(value);
+    for (let key in array) {
+        if (array[key] === value) result.push(value);
     }
     return result;
 };
@@ -35,8 +40,37 @@ const isInArrayReduce = (array, value) => {
     }, []);
 };
 
+const isInObjectForIn = (object, value) => {
+    const result = [];
+    for (let key in object) {
+        if (object[key] === value) result.push(object[key]);
+    }
+    return result;
+};
+
+const isInObjectFilter = (object, value) => {
+    return Object.keys(object).filter(key => {
+        if (object[key] === value) {
+            return true;
+        }
+    }).map(key => object[key]);
+};
+
+const isInObjectReduce = (object, value) => {
+    return Object.keys(object).reduce((result, key) => {
+        if (object[key] !== value) {
+            return result;
+        }
+        result.push(object[key]);
+        return result;
+    }, []);
+};
+
 benchmark.benchBatch([
     {fn: isInArrayForIn, args: [testArray, 'findme']},
     {fn: isInArrayFilter, args: [testArray, 'findme']},
     {fn: isInArrayReduce, args: [testArray, 'findme']},
+    {fn: isInObjectForIn, args: [testObject, 'findme']},
+    {fn: isInObjectFilter, args: [testObject, 'findme']},
+    {fn: isInObjectReduce, args: [testObject, 'findme']},
 ]);
