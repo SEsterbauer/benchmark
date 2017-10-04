664898f9fa8614477d5a48b63d525b8cfce04bb8
stefan <stefan.e@swell.wtf>
2017-10-05 00:58:49 +0200

    - indent reformat

--- benchmark.js
+++ benchmark.js
@@ -4,74 +4,74 @@ const Promise = require('bluebird');
 const logger = require('./logger');
 
 module.exports = {
-    /**
-     * Get pre-defined objects to iterate over in functions
-     * @param size {number} Size of iterator objects
-     * @returns {{testArray: [null], testObject: *}}
-     */
-    getTestIterators: size => {
-        logger.log('testing on testArray and testObject with length', size);
+  /**
+   * Get pre-defined objects to iterate over in functions
+   * @param size {number} Size of iterator objects
+   * @returns {{testArray: [null], testObject: *}}
+   */
+  getTestIterators: size => {
+    logger.log('testing on testArray and testObject with length', size);
 
-        const testArray = [...new Array(size).keys()];
-        testArray[size] = 'findme';
+    const testArray = [...new Array(size).keys()];
+    testArray[size] = 'findme';
 
-        const testObject = testArray.reduce((result, item, index) => {
-            result[index] = item;
-            return result;
-        }, {});
-        testObject[size] = 'findme';
+    const testObject = testArray.reduce((result, item, index) => {
+      result[index] = item;
+      return result;
+    }, {});
+    testObject[size] = 'findme';
 
-        return {
-            testArray,
-            testObject,
-        };
-    },
-    /**
-     * Benchmark a function
-     * @param fn {function} Function to benchmark
-     * @param args {any[]} Arguments to call fn with
-     * @return {Promise.<any>}
-     */
-    bench: (fn, args) => {
-        logger.time(fn.name);
-        return Promise.resolve()
-            .then(() => {
-                return fn(...args);
-            })
-            .then(result => {
-                logger.timeEnd(fn.name);
-                logger.log('function result was', result);
-                return result;
-            })
-            .catch(error => {
-                logger.error('Error during benchmark:', error);
-            });
-    },
-    /**
-     * Benchmark a batch of functions synchronously
-     * @param fns {object[]}
-     * @param fns[].fn {function} Function to benchmark
-     * @param fns[].args {any[]} Arguments to call fn with
-     * @return {Promise.<any>}
-     */
-    benchBatch: (fns) => {
-        logger.time('benchBatch');
-        return Promise.map(fns, fn => {
-            logger.time(fn.fn.name);
-            return Promise.resolve()
-                .then(() => fn.fn(...fn.args))
-                .then(result => {
-                    logger.timeEnd(fn.fn.name);
-                    return result;
-                });
-        }, { concurrency: 1 })
-            .then(results => {
-                logger.timeEnd('benchBatch');
-                logger.log('function results were', results.join(','));
-                return results;
-            })
-            .catch(error => {
-                logger.error('Error during benchmark batch', error);
-            });
-    },
+    return {
+      testArray,
+      testObject,
+    };
+  },
+  /**
+   * Benchmark a function
+   * @param fn {function} Function to benchmark
+   * @param args {any[]} Arguments to call fn with
+   * @return {Promise.<any>}
+   */
+  bench: (fn, args) => {
+    logger.time(fn.name);
+    return Promise.resolve()
+      .then(() => {
+        return fn(...args);
+      })
+      .then(result => {
+        logger.timeEnd(fn.name);
+        logger.log('function result was', result);
+        return result;
+      })
+      .catch(error => {
+        logger.error('Error during benchmark:', error);
+      });
+  },
+  /**
+   * Benchmark a batch of functions synchronously
+   * @param fns {object[]}
+   * @param fns[].fn {function} Function to benchmark
+   * @param fns[].args {any[]} Arguments to call fn with
+   * @return {Promise.<any>}
+   */
+  benchBatch: (fns) => {
+    logger.time('benchBatch');
+    return Promise.map(fns, fn => {
+      logger.time(fn.fn.name);
+      return Promise.resolve()
+        .then(() => fn.fn(...fn.args))
+        .then(result => {
+          logger.timeEnd(fn.fn.name);
+          return result;
+        });
+    }, { concurrency: 1 })
+      .then(results => {
+        logger.timeEnd('benchBatch');
+        logger.log('function results were', results.join(','));
+        return results;
+      })
+      .catch(error => {
+        logger.error('Error during benchmark batch', error);
+      });
+  },
 };
--- test.js
+++ test.js
@@ -8,58 +8,58 @@ const testArray = testIterators.testArray;
 const testObject = testIterators.testObject;
 
 const isInArrayForIn = (array, value) => {
-    const result = [];
-    for (let key in array) {
-        if (array[key] === value) result.push(value);
-    }
-    return result;
+  const result = [];
+  for (let key in array) {
+    if (array[key] === value) result.push(value);
+  }
+  return result;
 };
 
 const isInArrayFilter = (array, value) => {
-    return array.filter(item => item === value);
+  return array.filter(item => item === value);
 };
 
 const isInArrayReduce = (array, value) => {
-    return array.reduce((result, item) => {
-        if (item !== value) {
-            return result;
-        }
-        result.push(item);
-        return result;
-    }, []);
+  return array.reduce((result, item) => {
+    if (item !== value) {
+      return result;
+    }
+    result.push(item);
+    return result;
+  }, []);
 };
 
 const isInObjectForIn = (object, value) => {
-    const result = [];
-    for (let key in object) {
-        if (object[key] === value) result.push(object[key]);
-    }
-    return result;
+  const result = [];
+  for (let key in object) {
+    if (object[key] === value) result.push(object[key]);
+  }
+  return result;
 };
 
 const isInObjectFilter = (object, value) => {
-    return Object.keys(object).filter(key => {
-        if (object[key] === value) {
-            return true;
-        }
-    }).map(key => object[key]);
+  return Object.keys(object).filter(key => {
+    if (object[key] === value) {
+      return true;
+    }
+  }).map(key => object[key]);
 };
 
 const isInObjectReduce = (object, value) => {
-    return Object.keys(object).reduce((result, key) => {
-        if (object[key] !== value) {
-            return result;
-        }
-        result.push(object[key]);
-        return result;
-    }, []);
+  return Object.keys(object).reduce((result, key) => {
+    if (object[key] !== value) {
+      return result;
+    }
+    result.push(object[key]);
+    return result;
+  }, []);
 };
 
 benchmark.benchBatch([
-    {fn: isInArrayForIn, args: [testArray, 'findme']},
-    {fn: isInArrayFilter, args: [testArray, 'findme']},
-    {fn: isInArrayReduce, args: [testArray, 'findme']},
-    {fn: isInObjectForIn, args: [testObject, 'findme']},
-    {fn: isInObjectFilter, args: [testObject, 'findme']},
-    {fn: isInObjectReduce, args: [testObject, 'findme']},
+  { fn: isInArrayForIn, args: [testArray, 'findme'] },
+  { fn: isInArrayFilter, args: [testArray, 'findme'] },
+  { fn: isInArrayReduce, args: [testArray, 'findme'] },
+  { fn: isInObjectForIn, args: [testObject, 'findme'] },
+  { fn: isInObjectFilter, args: [testObject, 'findme'] },
+  { fn: isInObjectReduce, args: [testObject, 'findme'] },
 ]);
