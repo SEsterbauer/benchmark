4ea5fd6c5644fc7ccfcb4374b1524d86873458d9
stefan <s.esterbauer@gmx.at>
2018-09-16 23:48:30 +0200

    fixed cwd, rm echos

--- run_multi_versions.sh
+++ run_multi_versions.sh
@@ -6,8 +6,7 @@ then
   dir ../../tests
   exit
 fi
+cd ../..
 while read version; do
-  echo
-  echo Running Benchmark with Node version $version
-  ~/.nvm/versions/node/v$version/bin/node ../../tests/$1
-done < ../../node_versions
+  ~/.nvm/versions/node/v$version/bin/node ./tests/$1
+done < ./node_versions
