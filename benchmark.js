0ca3d69677e7e2aa5d5ac6ef50679dcbbd8762fe
stefan <s.esterbauer@gmx.at>
2018-09-16 23:54:53 +0200

    various improvements: - made 'findMe' value not exceed supplied size - improved logs - implemented optional logfile & .persistLogs parameter

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Promise = require('bluebird');
+const logger = require('./tools/Logger')(module);
 
 module.exports = {
   /**
@@ -8,21 +9,27 @@ module.exports = {
    */
   logOutput: false,
   /**
+   * Persist generated logs in a logfile if true
+   */
+  persistLogs: false,
+  /**
    * Get pre-defined objects to iterate over in functions
    * @param size {number} Size of iterator objects
    * @returns {{testArray: [null], testObject: *}}
    */
   getTestIterators: size => {
-    console.log('testing on testArray and testObject with length', size);
-
-    const testArray = [...new Array(size).keys()];
+    console.info('\nCreated test assets of size', size, '\n');
+    if (module.exports.persistLogs) {
+      logger.persistSync(`\nCreated test assets of size ${size}\n`);
+    }
+    const testArray = [...new Array(size - 1).keys()];
     testArray[size] = 'findme';
 
     const testObject = testArray.reduce((result, item, index) => {
       result[index] = item;
       return result;
     }, {});
-    testObject[size] = 'findme';
+    testObject[size - 1] = 'findme';
 
     return {
       testArray,
@@ -36,18 +43,27 @@ module.exports = {
    * @return {Promise.<any>}
    */
   bench: (fn, args) => {
-    console.time(fn.name);
+    const functionTimer = new Date().getTime();
     return Promise.resolve()
       .then(() => {
+        console.time(fn.name);
         return fn(...args);
       })
       .then(result => {
         console.timeEnd(fn.name);
-        console.log('function result was', result);
+        if (module.exports.persistLogs) {
+          logger.persistSync(`a- ${fn.fn.name}: ${new Date().getTime() - functionTimer}ms`);
+        }
+        if (module.exports.logOutput) {
+          console.info('Function result was', result);
+        }
         return result;
       })
       .catch(error => {
-        console.error('Error during benchmark:', error);
+        console.error(`Error while benchmarking function after ${new Date().getTime() - functionTimer}}ms: ${error.stack}`); // eslint-disable-line max-len
+        if (module.exports.persistLogs) {
+          logger.persistSync(`Error while benchmarking function after ${new Date().getTime() - functionTimer}}ms: ${error.stack}`); // eslint-disable-line max-len
+        }
       });
   },
   /**
@@ -58,25 +74,42 @@ module.exports = {
    * @return {Promise.<any>}
    */
   benchBatch: (fns) => {
-    console.time('benchBatch');
-    return Promise.map(fns, fn => {
+    if (module.exports.persistLogs) {
+      logger.persistSync(`Benchmarking batch with Node version ${process.version}:`);
+    }
+    console.info(`Benchmarking batch with Node version ${process.version}:`)
+    const batchTimer = new Date().getTime();
+    console.time('Total');
+    return Promise.each(fns, fn => {
+      const functionTimer = new Date().getTime();
       console.time(fn.fn.name);
       return Promise.resolve()
         .then(() => fn.fn(...fn.args))
         .then(result => {
           console.timeEnd(fn.fn.name);
-          return result;
+          if (module.exports.persistLogs) {
+            logger.persistSync(`\t${fn.fn.name}: ${new Date().getTime() - functionTimer}ms`);
+          }
+          return module.exports.logOutput ? result : null;
         });
-    }, { concurrency: 1 })
+    })
       .then(results => {
-        console.timeEnd('benchBatch');
-        if (module.exports.logOutput) {
-          console.log('function results were', results.join(','));
+        console.timeEnd('Total');
+        if (module.exports.persistLogs) {
+          logger.persistSync(`Whole function batch took: ${new Date().getTime() - batchTimer}ms\n\n____________________________________________________\n\n`); // eslint-disable-line max-len
         }
-        return results;
+        if (module.exports.logOutput) {
+          console.info(`Function results were; ${results.join(',')}`);
+        }
+        console.info();
       })
       .catch(error => {
-        console.error('Error during benchmark batch', error);
+        console.error(`Error while benchmarking batch after ${new Date().getTime() - batchTimer}}ms: ${error.stack}`);
+        if (module.exports.persistLogs) {
+          logger.persistSync(`Error while benchmarking batch after ${new Date().getTime() - batchTimer}}ms: ${error.stack}\n\n____________________________________________________\n\n`); // eslint-disable-line max-len
+        }
+        console.info();
       });
   },
+  logger,
 };
